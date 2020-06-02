18a8d95271def0393b139b010bae60231c51947b
ShowTimeU <d.yankovych@gmail.com>
2020-06-02 21:57:59 +0300

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -64,7 +64,7 @@ import {MatStepperModule} from '@angular/material/stepper';
     MatSidenavModule,
     MatTabsModule,
     FormsModule,
-    ChatModule,
+    ChatModule
   ],
   providers: [ChatService],
   bootstrap: [AppComponent]
